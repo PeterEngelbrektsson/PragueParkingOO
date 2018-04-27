5feb1c0d7ebd4bb7057a32a84d1c3a224446d636
Peter Engelbrektsson <peter.engelbrektsson@hotmail.com>
2018-04-27 09:14:22 +0200

    Added storage slot details sort by

---
+++ MyCompany/Storage.Biz/StorageSlotDetail_SortBy.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany.Storage.Biz
+{
+    /// <summary>
+    /// Sort on storage slot number in ascending order.
+    /// </summary>
+    public class StorageSlotDetail_SortByStorageSlotNumberAscendingOrder<T> : IComparer<StorageSlotDetail<T>> where T : IStoreable
+    {
+        public int Compare(StorageSlotDetail<T> x, StorageSlotDetail<T> y)
+        {
+            if (x.SlotNumber < y.SlotNumber) return -1;
+            else if (x.SlotNumber > y.SlotNumber) return 1;
+            else return 0;
+        }
+    }
+    /// <summary>
+    /// Sort on storage slot number in descending order.
+    /// </summary>
+    public class StorageSlotDetail_SortByStorageSlotNumberDescendingOrder<T> : IComparer<StorageSlotDetail<T>> where T : IStoreable
+    {
+        public int Compare(StorageSlotDetail<T> x, StorageSlotDetail<T> y)
+        {
+            if (x.SlotNumber > y.SlotNumber) return -1;
+            else if (x.SlotNumber < y.SlotNumber) return 1;
+            else return 0;
+        }
+    }
+    /// <summary>
+    /// Sort on occupied space in ascending order.
+    /// </summary>
+    public class StorageSlotDetail_SortByOccupiedSpaceAscendingOrder<T> : IComparer<StorageSlotDetail<T>> where T : IStoreable
+    {
+        public int Compare(StorageSlotDetail<T> x, StorageSlotDetail<T> y)
+        {
+            if (x.OccupiedSpace < y.OccupiedSpace) return -1;
+            else if (x.OccupiedSpace > y.OccupiedSpace) return 1;
+            else return 0;
+        }
+    }
+    /// <summary>
+    /// Sort on occupied space in ascending order.
+    /// </summary>
+    public class StorageSlotDetail_SortByOccupiedSpaceDescendingOrder<T> : IComparer<StorageSlotDetail<T>> where T : IStoreable
+    {
+        public int Compare(StorageSlotDetail<T> x, StorageSlotDetail<T> y)
+        {
+            if (x.OccupiedSpace > y.OccupiedSpace) return -1;
+            else if (x.OccupiedSpace < y.OccupiedSpace) return 1;
+            else return 0;
+        }
+    }
+
+    /// <summary>
+    /// Sort on free space in ascending order.
+    /// </summary>
+    public class StorageSlotDetail_SortByFreeSpaceAscendingOrder<T> : IComparer<StorageSlotDetail<T>> where T : IStoreable
+    {
+        public int Compare(StorageSlotDetail<T> x, StorageSlotDetail<T> y)
+        {
+            if (x.FreeSpace< y.FreeSpace) return -1;
+            else if (x.FreeSpace > y.FreeSpace) return 1;
+            else return 0;
+        }
+    }
+    /// <summary>
+    /// Sort on free space in ascending order.
+    /// </summary>
+    public class StorageSlotDetail_SortByFreeSpaceDescendingOrder<T> : IComparer<StorageSlotDetail<T>> where T : IStoreable
+    {
+        public int Compare(StorageSlotDetail<T> x, StorageSlotDetail<T> y)
+        {
+            if (x.FreeSpace > y.FreeSpace) return -1;
+            else if (x.FreeSpace < y.FreeSpace) return 1;
+            else return 0;
+        }
+    }
+ 
+}
